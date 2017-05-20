d0d30cdad50fd6f37677440d67c98991121c593b
Jacob Ward <jacob@candyboxmedia.com>
2017-05-20 15:13:15 +0100

    Add xo linting and refactoring

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+/* eslint eqeqeq: 0 */
 'use strict';
 
 function looseArrayEquals(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
-    return false;
-  }
+	if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
+		return false;
+	}
 
-  return arr1.every(function(item, index) {
-    return item == arr2[index];
-  });
+	return arr1.every((item, index) => {
+		return item == arr2[index];
+	});
 }
 
 module.exports = looseArrayEquals;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,15 +1,16 @@
+/* global describe it expect */
 'use strict';
 
-var looseArrayEquals = require('../index');
+const looseArrayEquals = require('../index');
 
-describe('looseArrayEquals', function() {
-  it('should return true when arrays are equal using abstract equality comparison', function() {
-    expect(looseArrayEquals(['a', '23'], ['a', 23])).toBe(true);
-    expect(looseArrayEquals([1, !![], !!{}, 0], [true, true, true, false])).toBe(true);
-  });
+describe('looseArrayEquals', () => {
+	it('should return true when arrays are equal using abstract equality comparison', () => {
+		expect(looseArrayEquals(['a', '23'], ['a', 23])).toBe(true);
+		expect(looseArrayEquals([1, Boolean([]), Boolean({}), 0], [true, true, true, false])).toBe(true);
+	});
 
-  it('should return false when arrays are not equal using abstract or strict equality comparison', function() {
-    expect(looseArrayEquals(['a', 23], [23, 'a'])).toBe(false);
-    expect(looseArrayEquals(['a', 23], [42])).toBe(false);
-  });
+	it('should return false when arrays are not equal using abstract or strict equality comparison', () => {
+		expect(looseArrayEquals(['a', 23], [23, 'a'])).toBe(false);
+		expect(looseArrayEquals(['a', 23], [42])).toBe(false);
+	});
 });
