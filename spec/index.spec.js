843c6076d6c7e3dbc53158c2525d5bb531106a7a
Jacob Ward <jacob@candyboxmedia.com>
2017-05-29 18:53:11 +0100

    add test coverage for all execution paths

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -13,4 +13,14 @@ describe('looseArrayEquals', () => {
 		expect(looseArrayEquals(['a', 23], [23, 'a'])).toBe(false);
 		expect(looseArrayEquals(['a', 23], [42])).toBe(false);
 	});
+
+	it('should return false when either argument is not an array', () => {
+		expect(looseArrayEquals(null, [23, 'a'])).toBe(false);
+		expect(looseArrayEquals(['a', 23], Function.prototype)).toBe(false);
+	});
+
+	it('should return false when arrays are not of equal length', () => {
+		expect(looseArrayEquals(['a', 23, 42], [23])).toBe(false);
+		expect(looseArrayEquals([23], ['a', 23, 42])).toBe(false);
+	});
 });
